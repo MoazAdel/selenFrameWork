ad80c13d357c82d59445b4e9986611a1525368fe
MoazAdel <moazadel116@yahoo.com>
2019-09-18 15:56:11 +0200

    Add Chrome Headless

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -17,19 +18,28 @@ public class TestBase extends AbstractTestNGCucumberTests {
     public static WebDriver driver;
 
     @BeforeSuite
-    //@Parameters({"browser" })
-    public void startDriver() {
+    @Parameters({"browser"})
+    public void startDriver(String browserName) {
 
-//        if(browserName.equalsIgnoreCase("chrome")) {
-//            driver = new ChromeDriver();
-//        }   else if (browserName.equalsIgnoreCase("firefox")) {
-//           driver = new FirefoxDriver();
-//        } else if(browserName.equalsIgnoreCase("ie")) {
-//            driver = new InternetExplorerDriver();
-//        } else if(browserName.equalsIgnoreCase("edge")) {
-//            driver = new EdgeDriver();
-//        }
-        driver = new FirefoxDriver();
+        if(browserName.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        }   else if (browserName.equalsIgnoreCase("firefox")) {
+           driver = new FirefoxDriver();
+        } else if(browserName.equalsIgnoreCase("ie")) {
+            driver = new InternetExplorerDriver();
+        } else if(browserName.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        } else if (browserName.equalsIgnoreCase("chrome-headless")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+            driver = new ChromeDriver(options);
+
+        }
+//        driver = new FirefoxDriver();
+
+        //Headless Browser Testing
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
         driver.navigate().to("https://demo.nopcommerce.com/");
